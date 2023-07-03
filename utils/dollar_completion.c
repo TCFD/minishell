0109f27cef2dbd7726baed212b960bee6e4a8e78
TCFD <r.ciaze@gmail.com>
2023-07-03 20:40:29 +0200

    dollar simple

--- utils/dollar_completion.c
+++ utils/dollar_completion.c
@@ -6,7 +6,7 @@
 /*   By: zbp15 <zbp15@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 16:34:39 by rciaze            #+#    #+#             */
-/*   Updated: 2023/07/03 19:50:38 by zbp15            ###   ########.fr       */
+/*   Updated: 2023/07/03 20:39:45 by zbp15            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*check_env_variables(char *input)
 	if (end <= 0)
 		end = ft_strlen(input);
 	value = ft_substr(input, 0, end);
-	if (!ft_strncmp(value, "$?", ft_strlen(value)))
+	if (ft_strlen(value) != 1 && !ft_strncmp(value, "$?", ft_strlen(value)))
 		return (free(value), ft_itoa(errno));
 	return_value = ft_strdup(ft_getenv(value + 1));
 	free (value);
